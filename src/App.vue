8014e9d9524884986a705bae6aa486cbc3544eff
Hayato Kamiyama <kami8ma8810@gmail.com>
2023-02-03 23:07:18 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <h1>Vue3 Easing Tool GUI</h1>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
